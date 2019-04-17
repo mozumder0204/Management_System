93be97526d0e3b1e5995c87290be9637db5394aa
Md. Billal Hossain Mozumder <mozumder0204@gmail.com>
2019-04-17 10:20:06 +0600

    sql

--- sql/db_management.sql
+++ sql/db_management.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 17, 2019 at 05:34 AM
+-- Generation Time: Apr 17, 2019 at 06:19 AM
 -- Server version: 10.1.35-MariaDB
 -- PHP Version: 7.2.9
 
